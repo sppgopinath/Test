0477bf0c4f2b4376e83ea55f8e0e87990ab5c684
sppgopinath <spgopinath@gmail.com>
2017-10-31 22:49:03 +0530

    Add files via upload

---
+++ src/main/java/com/spg/example/mylearning/MylearningApplication.java
@@ -0,0 +1,12 @@
+package com.spg.example.mylearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylearningApplication.class, args);
+	}
+}
